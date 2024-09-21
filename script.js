f2ef84a20d5883f85b0f75d8ff96b26e2dde4611
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2024-09-21 07:39:07 +0530

    stop-event-bubbling-from-navbar-container

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ const horiontalLines = document.querySelector('#horisontal-lines-id')
 const listOptions = document.querySelector('.nav-options')
 const navBarContainer = document.querySelector('.navbar-container')
 
-horiontalLines.addEventListener('click',(e)=>{
+horiontalLines.addEventListener('click', (e) => {
     e.stopPropagation()
     e.target.parentElement.classList.toggle('expand')
 
-    if([...e.target.parentElement.classList].includes('expand')){
-        setTimeout(()=>{
+    if ([...e.target.parentElement.classList].includes('expand')) {
+        setTimeout(() => {
             listOptions.classList.remove('display-none-phone')
-        },300)
+        }, 300)
 
     }
-    else{
+    else {
         listOptions.classList.add('display-none-phone')
 
     }
     e.target.classList.toggle('position-lock')
 })
 
-scrollUp.addEventListener('click',(e)=>{
+scrollUp.addEventListener('click', (e) => {
     e.stopPropagation()
     window.scrollTo({
-        top : -(document.body.scrollHeight),
+        top: -(document.body.scrollHeight),
         behavior: 'smooth'
     })
 })
 
-// navBarContainer.addEventListener('click',(e)=>{
-//     e.stopPropagation()
-// })
+navBarContainer.addEventListener('click',(e)=>{
+    e.stopPropagation()
+})
 
-window.addEventListener('click',()=>{
-        horiontalLines.parentElement.classList.remove('expand')
-        horiontalLines.classList.remove('position-lock')
-        listOptions.classList.add('display-none-phone')
+window.addEventListener('click', () => {
+    horiontalLines.parentElement.classList.remove('expand')
+    horiontalLines.classList.remove('position-lock')
+    listOptions.classList.add('display-none-phone')
 })
